168edce10290f9e6e34803acff549cf371d6f450
xiangacadia <xiangjiang.acadia@gmail.com>
2014-05-05 18:51:08 -0300

    revised month2year

--- preprocessing/missing value/monthly2yearly.R
+++ preprocessing/missing value/monthly2yearly.R
@@ -1,9 +1,13 @@
+
 yeardata <- c()
-for(year in c(1:13)){
+yearNum <- 20
+years <- c(1:yearNum)
+for(year in years){
+    year<-1
     temp <- rep(0, ncol(result))
     for(i in c(1:12)){temp <- temp + result[,i+2+(year-1)*12]}
     yeardata <- cbind(yeardata, temp)
 }
-yeardata <- cbind(data[,1:startColumn-1], yeardata)
-names(yeardata)[startColumn:ncol(yeardata)] <- c("01","02","03","04","05","06","07","08","09","10","11","12","13")
-write.csv(yeardata, "/Users/xiangjiang/Documents/temp/Employee number/2.[no missing value-yearly] Employee number.csv", row.names = FALSE)
+yeardata <- cbind(result[,1:startColumn-1], yeardata)
+names(yeardata)[startColumn:ncol(yeardata)] <- as.character(1993+years)
+write.csv(yeardata, "/Users/xiangjiang/Documents/temp/data/Umemployment rate/2.[no missing value-yearly]Unemployment-rate-new.csv", row.names = FALSE)
