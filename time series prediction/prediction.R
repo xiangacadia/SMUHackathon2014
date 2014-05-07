696f9b7a6e12c60f467599d0f561e35cbb300f35
xiangacadia <xiangjiang.acadia@gmail.com>
2014-05-07 13:45:39 -0300

    1st runable version

--- time series prediction/prediction.R
+++ time series prediction/prediction.R
@@ -1,16 +1,19 @@
 library(forecast)
 
-data <- read.csv("C:\\Users\\ML3Lab\\Dropbox\\SMU Hackathon 2014\\data\\Average income\\7.[time series-window size-5]Average income.csv")
-#value <- c(1.2,1.7,1.6, 1.2, 1.6, 1.3, 1.5, 1.9, 5.4, 4.2, 5.5, 6.0, 5.6, 6.2, 6.8, 7.1, 7.1, 5.8, 0.0, 5.2, 4.6, 3.6, 3.0, 3.8, 3.1, 3.4, 2.0, 3.1, 3.2, 1.6, 0.6, 3.3, 4.9, 6.5, 5.3, 3.5, 5.3, 7.2, 7.4, 7.3, 7.2, 4.0, 6.1, 4.3, 4.0, 2.4, 0.4, 2.4, 1.2,1.7,1.6, 1.2, 1.6, 1.3, 1.5, 1.9, 5.4, 4.2, 5.5, 6.0, 5.6, 6.2, 6.8, 7.1, 7.1, 5.8, 0.0, 5.2, 4.6, 3.6, 3.0, 3.8, 3.1, 3.4, 2.0, 3.1, 3.2, 1.6, 0.6, 3.3, 4.9, 6.5, 5.3, 3.5, 5.3, 7.2, 7.4, 7.3, 7.2, 4.0, 6.1, 4.3, 4.0, 2.4, 0.4, 2.4)
+data <- read.csv("C:\\Users\\ML3Lab\\Dropbox\\SMU Hackathon 2014\\data\\Average income\\3.[no missing value-yearly]Average income.csv")
 startCol <- 3
 endCol <- ncol(data)
+
+yearsAhead <- 2018 - 2013
+
+predictions <- c()
 
 for(rowNum in c(1:nrow(data))){
 
 value <- as.integer(data[rowNum,startCol:endCol])
 ## create time series objects
 ## frequencty: the number of observations per unit of time
-series <- ts(value,frequency=1) # consider adding a start so you get nicer labelling on your chart. 
+series <- ts(value,frequency=1)
 
 ## Fit best ARIMA model to univariate time series
 ## Returns best ARIMA model according to either AIC, AICc or BIC value. 
@@ -19,4 +22,11 @@ fit <- auto.arima(series)
 
 fcast <- forecast(fit)
 
+fcast$mean[fcast$mean<0] <- 0
+
+predictions <- append(predictions, fcast$mean[yearsAhead])
 }
+
+data <- cbind(data[,c(1,2,ncol(data))], predictions)
+
+write.csv(data,"C:\\Users\\ML3Lab\\Dropbox\\SMU Hackathon 2014\\predictions\\Average income.csv")
